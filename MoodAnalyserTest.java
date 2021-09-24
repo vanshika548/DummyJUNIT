cf7877171bf6e252275bf2db1cc78dd1b4e76ffa
unknown <vanshikarastogi689@gmail.com>
2021-09-24 16:58:35 +0530

    [Vanshika]Added:DummyJUNIT

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoodAnalyserTest {
+
+	@Test
+	void testAnalyseMood() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+	}
+
+}
